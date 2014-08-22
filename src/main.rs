7c07c5dcf4219149c4409fad8687689cd0a4c6e5
Thông Nguyễn <xcodevn@gmail.com>
2014-08-22 16:44:52 +0800

    Added check prime function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// check if the number is prime or not
+fn is_prime_number(num: uint) -> bool {
+    if num < 2 { return false; }
+    
+    let mut count : uint = 2;
+    while count * count < num {
+        if num % count == 0 { return false; }
+        count = count + 1;
+    }
+
+    return true;
+}
+
 fn main() {
     println!("Hello, world!")
 }
